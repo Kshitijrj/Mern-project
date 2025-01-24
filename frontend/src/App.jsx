f4d2dbf800ff6279d3b8b4a87333487774444348
Kshitij Raj <kshitijraj89@gmail.com>
2025-01-24 22:14:46 +0530

    Inital commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import CreatePage from "./pages/Createpage";
+import CreatePage from "./pages/CreatePage"
 import HomePage from "./pages/HomePage"
 import Navbar from "./components/Navbar";
 
